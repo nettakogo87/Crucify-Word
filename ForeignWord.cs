a4fe6625e65e0f15b8a5793f9709d2b0f34075f0
Vladimir <bratstakan@gmail.com>
2012-11-01 15:51:25 +0300

    Card превратился в ForeignWord, в связи с уточнением предметной области.

--- ForeignWord.cs
+++ ForeignWord.cs
@@ -3,28 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 /**
- * Класс Карточки Английского слова. Содержит уникальный ID и уникальное слово. 
+ * Класс Иностранное слово. Содержит уникальный ID и уникальное слово. 
  * В данный момент содержит только один перевод. Но в будуйщем переводы можно будет добавлять.
  */
 namespace Crucify_Word
 {
-    public class Card
+    public class ForeignWord
     {
-        public Card(string word, string translation):this(word, translation, String.Empty) {}
-        public Card(string word, string translation, string transcription)
+        public ForeignWord(string word, string translation):this(word, translation, String.Empty) {}
+        public ForeignWord(string word, string translation, string transcription)
         {
             this.id = 0; // придумать очень сложный способ вычисления нового id
             this.word = word;
             this.transcription = transcription;
-            this.translations = new List<Translation>();
             Translation translationOfCard = new Translation(translation);
-            this.translations.Add(translationOfCard);
+            this.translations = new TranslationList(translationOfCard);
         }
 
         private Int64 id;
         private string word;
         private string transcription;
-        private List<Translation> translations;
+        private TranslationList translations;
 
         public Int64 Id
         {
@@ -38,7 +37,7 @@ namespace Crucify_Word
         {
             get { return transcription; }
         }
-        public List<Translation> Translations
+        public TranslationList Translations
         {
             get { return translations; }
         }
