a425433fdea00b10d8ce23ffe030dd66ab81dded
Vladimir <bratstakan@gmail.com>
2012-11-01 13:07:47 +0300

    Созданн класс для хранения Русского слова - перевода Английского.

---
+++ Translation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/**
+ * Класс представляетс собой Русское слово - перевод Английского. 
+ * В последующем предполагается что будет хранить идентификатор популярности данного перевода.
+ */
+namespace Crucify_Word
+{
+    public class Translation
+    {
+        public Translation(string word)
+        {
+            this.word = word; 
+        }
+        private string word;
+        public string Word
+        {
+            get { return word; }
+            set { word = value; }
+        }
+    }
+}
