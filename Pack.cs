26a129b73386d26fa246bab1e3ace95feab9a66e
Vladimir <bratstakan@gmail.com>
2012-11-07 10:04:30 +0300

    Создан класс колоды для хранения карточек

---
+++ Pack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+/**
+ * Колода в которой хранятся карточки. 
+ * Экземпляры этого класса будут представлять собой начальную колоду и колоду изьятия
+ */
+namespace Crucify_Word
+{
+    public class Pack 
+    {
+        protected int packNumber;
+        protected CardList cardList;
+        public int PackNumber
+        {
+            get { return this.packNumber; }
+        }
+        public void AddCard(Card newCard)
+        {
+            this.cardList.AddCard(newCard);
+        }
+        public void RemoveCard(Card oldCard)
+        {
+            this.cardList.RemoveCard(oldCard);
+        }
+    }
+}
