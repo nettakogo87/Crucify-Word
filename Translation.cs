15f7498c04928b3fc104af8658657eaa20b336c5
Vladimir <bratstakan@gmail.com>
2012-11-28 17:52:07 +0300

    В транскрипцию добавленны функции загрузки и сохранения

--- Translation.cs
+++ Translation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 /**
  * Класс представляетс собой Русское слово - перевод Английского. 
  * В последующем предполагается что будет хранить идентификатор популярности данного перевода.
@@ -12,13 +13,28 @@ namespace Crucify_Word
     {
         public Translation(string word)
         {
-            this.word = word; 
+            this.word = word;
+            this.save();
         }
         private string word;
         public string Word
         {
             get { return word; }
-            set { word = value; }
+            set { word = value; this.save(); }
+        }
+        public void save()
+        {
+            FileInfo newWord = new FileInfo(@"voc\translations\"+this.word);
+            StreamWriter sw = newWord.CreateText();
+            sw.WriteLine(this.word);
+            sw.Close();
+        }
+        public void load()
+        {
+            FileInfo newWord = new FileInfo(this.word);
+            StreamReader sw = newWord.OpenText();
+            this.word = sw.ReadLine();
+            sw.Close();
         }
     }
 }
