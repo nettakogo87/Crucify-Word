1108a8fc08628f7acf040791872a25b57e15743d
Vladimir <bratstakan@gmail.com>
2012-11-28 16:08:44 +0300

    В класс карточки добавлены поля даты дней повторений. При прогрессе слова, дата пересчитывается, при регрессе тоже. Добавлена функция инициализации, для того что бы быть уверенным, что слово пройдет тренировку, даже если его дата уже прошла.

--- Card.cs
+++ Card.cs
@@ -6,19 +6,25 @@ using System.Text;
  * Класс Card представляет собой карточку которая содержит иностранное слово и перевод к ниму с другой стороны.
  * Так же карточка хронит прогресс которого добился пользоватьель в изученнии данного слова.
  * И конечно же номер колоды в которой на данный момент находится карточка.
+ * Дата предидущего повторения, и следующего повторения добавленны для того что бы было удобно определить,
+ * кодгда нужно повторить карточку.
  */
 namespace Crucify_Word
 {
     public class Card
     {
+        public const int PACKS_COUNT = 7; // колод повторения всего 7, не считая колоды изьятия
+        public const int PROGRESS_MAX = 6; // максимальный прогресс при котором слово считается изученным
         public Card(ForeignWord cardWord, string selectTranslation)
         {
             this.id = fake_id++; // придумать как получать новые айдишники.
-            this.packNumber = 0;
+            this.packNumber = Convert.ToInt16(DateTime.Now.DayOfWeek) + 1; // номера колод соответствуют дням недели
             this.cardWord = cardWord;
             this.selectTranslation = selectTranslation;
             this.cardWord.Translations.AddTranslationByWord(selectTranslation);
-            this.progress = 0;
+            this.progress = 1;
+            this.lastRepeating = DateTime.Now;
+            this.newRepeating = DateTime.Now;
         }
         private static int fake_id;
         private Int64 id;
@@ -26,6 +32,8 @@ namespace Crucify_Word
         private ForeignWord cardWord;
         private string selectTranslation;
         private int progress;
+        private DateTime lastRepeating;
+        private DateTime newRepeating;
 
         public ForeignWord CardWord
         {
@@ -42,32 +50,54 @@ namespace Crucify_Word
         }
         public void IncrementProgress()
         {
-            if (5 > this.progress)
+            if (PROGRESS_MAX > this.progress)
             {
                 this.progress++;
+                this.packNumber += this.progress;
+                if (PACKS_COUNT < this.packNumber)
+                {
+                    this.packNumber -= PACKS_COUNT;
+                }
+                this.lastRepeating = DateTime.Now;
+                this.newRepeating = DateTime.Now.AddDays(this.progress);
             }
         }
         public void DecrementProgress()
         {
             if (0 < this.progress)
             {
+                this.packNumber -= this.progress;
+                if (0 > this.packNumber)
+                {
+                    this.packNumber += PACKS_COUNT;
+                }
                 this.progress--;
+                this.lastRepeating = DateTime.Now;
+                this.newRepeating = DateTime.Now.AddDays(this.progress);
             }
         }
         public void toExplore()
         {
-            this.packNumber = 0;
-            this.progress = 0;
+            this.packNumber = Convert.ToInt16(DateTime.Now.DayOfWeek) + 1; // номера колод соответствуют дням недели;
+            this.progress = 1;
         }
         public void markAsLearned()
         {
-            this.packNumber = 11;
-            this.progress = 5;
+            this.packNumber = PACKS_COUNT + 1; // в колоду изьятия
+            this.progress = PROGRESS_MAX;
         }
         public int PackNumber
         {
             get { return this.packNumber; }
             set { this.packNumber = value; }
+        }
+        public void initCard()
+        {
+            if (this.newRepeating < DateTime.Now)
+            {
+                this.newRepeating = DateTime.Now;
+                this.packNumber = Convert.ToInt16(DateTime.Now.DayOfWeek) + 1; // номера колод соответствуют дням недели;
+            }
         }
     }
 }
