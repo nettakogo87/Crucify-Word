136b89fc9e1c33733f9f8cca64c5029906a1cd84
Vladimir <bratstakan@gmail.com>
2012-11-30 17:50:42 +0300

    [Program.cs] Убрал создание лишних папок

--- Program.cs
+++ Program.cs
@@ -14,28 +14,6 @@ namespace Crucify_Word
         [STAThread]
         static void Main()
         {
-
-            DirectoryInfo voc = new DirectoryInfo(@"voc");
-            if (!voc.Exists)
-            {
-                voc.Create();
-            }
-            DirectoryInfo translations = new DirectoryInfo(@"voc\translations");
-            if (!translations.Exists)
-            {
-                translations.Create();
-            }
-            DirectoryInfo cards = new DirectoryInfo(@"voc\cards");
-            if (!cards.Exists)
-            {
-                cards.Create();
-            }
-            DirectoryInfo foreignWords = new DirectoryInfo(@"voc\foreign_words");
-            if (!foreignWords.Exists)
-            {
-                foreignWords.Create();
-            }
-
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
