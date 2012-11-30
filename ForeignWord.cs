5c5227f3945defb845673678fe6267e7ab0b4169
Vladimir <bratstakan@gmail.com>
2012-11-30 15:27:44 +0300

    В иностранное слово добавлены функции загрузки и сохранения

--- ForeignWord.cs
+++ ForeignWord.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 /**
  * Класс Иностранное слово. Содержит уникальный ID и уникальное слово. 
  * В данный момент содержит только один перевод. Но в будуйщем переводы можно будет добавлять.
@@ -21,6 +22,14 @@ namespace Crucify_Word
         public const int CONJUNCTION = 9; // союзы
         public const int NOT_KNOW = 10; // в случае если человек затрудняется выбрать
 
+        public ForeignWord()
+        {
+            this.id = 0; // придумать очень сложный способ вычисления нового id
+            this.word = "";
+            this.transcription = "";
+            this.translations = new TranslationList();
+            this.partOfSpeech = 10;
+        }
         public ForeignWord(string word, string translation) : this(word, translation, String.Empty, ForeignWord.NOT_KNOW) { }
         public ForeignWord(string word, string translation, int partOfSpeech) : this(word, translation, String.Empty, partOfSpeech) { }
         public ForeignWord(string word, string translation, string transcription) : this(word, translation, transcription, ForeignWord.NOT_KNOW) { }
@@ -59,6 +68,29 @@ namespace Crucify_Word
         public int PartOfSpeech
         {
             get { return partOfSpeech; }
+        }
+        public void save()
+        {
+            FileInfo newWordDoc = new FileInfo(@"voc\foreign_words\" + this.id + ".txt");
+            StreamWriter sw = newWordDoc.CreateText();
+            sw.WriteLine(this.id);
+            sw.WriteLine(this.word);
+            sw.WriteLine(this.transcription);
+            sw.WriteLine(this.partOfSpeech);
+            sw.Close();
+            this.translations.save();
+            
+        }
+        public void load(string Id)
+        {
+            FileInfo newWordDoc = new FileInfo(Id);
+            StreamReader sw = newWordDoc.OpenText();
+            this.id = Convert.ToInt16(sw.ReadLine());
+            this.word = sw.ReadLine();
+            this.transcription = sw.ReadLine();
+            this.partOfSpeech = Convert.ToInt16(sw.ReadLine());
+            sw.Close();
+            this.translations.load();
         }
     }
 }
