c619676237a35faabfd4e46a8cdd21a684927267
Vladimir <bratstakan@gmail.com>
2012-11-30 23:09:11 +0300

    В словарь добавлены функция сохранения карточки по id, загрузка всех карточек, поиск максимального id карточки и инициализация словаря

--- Vocabulary.cs
+++ Vocabulary.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 /**
  * Хранит все карточки пользователя
  * Пока можно добавить карточку или удалить, а еще получить список карточек.
@@ -47,7 +48,61 @@ namespace Crucify_Word
             return this.cards.Count;
         }
 
+        public void saveCardById(int id)
+        {
+            foreach (Card card in this.cards)
+            {
+                if (card.Id == id)
+                {
+                    FileInfo saveWord = new FileInfo(@"vocabulary\cards\" + id + ".txt");
+                    StreamWriter sw = saveWord.CreateText();
+                    sw.WriteLine(card.ForeignWord);
+                    sw.WriteLine(card.Transcription);
+                    sw.WriteLine(card.Translation);
+                    sw.WriteLine(card.PackNumber);
+                    sw.WriteLine(card.Progress);
+                    sw.WriteLine(card.Id);
+                    sw.WriteLine(card.LastRepeating);
+                    sw.WriteLine(card.NewRepeating);
+                    sw.Close();
+                    break;
+                }
+            }
+        }
+
+        public void InitVocabulary()
+        {
+            DirectoryInfo vocabularyDir = new DirectoryInfo(@"vocabulary\cards");
+            if (vocabularyDir.Exists)
+            {
+                this.cards.RemoveRange(0, this.cards.Count);
+                this.loadAllCards();
+                foreach (Card card in this.cards)
+                {
+                    card.initCard();
+                }
+            }
+            else
+            {
+                vocabularyDir.Create();
+            }
+        }
         // приватные методы 
+
+        private void loadAllCards()
+        {
+            DirectoryInfo vocabularyDir = new DirectoryInfo(@"vocabulary\cards");
+            for (int i = 0; i < vocabularyDir.GetFiles().Length; i++)
+            {
+                FileInfo loadWord = new FileInfo(@"vocabulary\cards\" + vocabularyDir.GetFiles()[i].Name);
+                StreamReader sr = loadWord.OpenText();
+                Card newCard = new Card(sr.ReadLine(), sr.ReadLine(), sr.ReadLine(), sr.ReadLine(), sr.ReadLine(), sr.ReadLine(), sr.ReadLine(), sr.ReadLine());
+                sr.Close();
+                this.AddCardToVocabulary(newCard);
+            }
+            Card.inc_id = this.GetCardMaxId();
+        }
+
         private bool ExistCard(Card newCard)
         {
             return this.cards.Exists(
@@ -65,6 +120,18 @@ namespace Crucify_Word
                     return card.ForeignWord == word && card.Translation == translation;
                 }
             );
+        }
+        private int GetCardMaxId()
+        {
+            int maxId = 0;
+            foreach (Card card in this.cards)
+            {
+                if (maxId < card.Id)
+                {
+                    maxId = card.Id;
+                }
+            }
+            return maxId;
         }
     }
 }
