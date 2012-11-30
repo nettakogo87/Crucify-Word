1d05d0f943ef388b3194298ed3c8523053edcfc4
Vladimir <bratstakan@gmail.com>
2012-11-30 15:28:20 +0300

    К переводу добавлены функции загрузки и сохранения

--- Translation.cs
+++ Translation.cs
@@ -11,28 +11,31 @@ namespace Crucify_Word
 {
     public class Translation
     {
+        public Translation()
+        {
+            this.word = "";
+        }
         public Translation(string word)
         {
             this.word = word;
-            this.save();
         }
         private string word;
         public string Word
         {
             get { return word; }
-            set { word = value; this.save(); }
+            set { word = value; }
         }
         public void save()
         {
-            FileInfo newWord = new FileInfo(@"voc\translations\"+this.word);
-            StreamWriter sw = newWord.CreateText();
+            FileInfo newWordDoc = new FileInfo(@"voc\translations\" + this.word + ".txt");
+            StreamWriter sw = newWordDoc.CreateText();
             sw.WriteLine(this.word);
             sw.Close();
         }
-        public void load()
+        public void load(string word)
         {
-            FileInfo newWord = new FileInfo(this.word);
-            StreamReader sw = newWord.OpenText();
+            FileInfo newWordDoc = new FileInfo(word);
+            StreamReader sw = newWordDoc.OpenText();
             this.word = sw.ReadLine();
             sw.Close();
         }
